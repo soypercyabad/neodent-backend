3693a232eeaccfa03850ed6f8c6623168f5d1b2d
soypercyabad <percyulisesmolinaabad@gmail.com>
2026-09-06 04:04:30 -0500

    chore: initialize Neodent backend

---
+++ src/main/java/com/neodent/NeodentBackendApplication.java
@@ -0,0 +1,13 @@
+package com.neodent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeodentBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NeodentBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/neodent/NeodentBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.neodent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeodentBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
